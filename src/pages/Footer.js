394d37cbf3ab278d56f4dc210f152633f9745823
Lucassec1 <106749169+Lucassec1@users.noreply.github.com>
2022-06-23 11:44:37 -0300

    Update Footer.js

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import api from '../api'
+import api from './api'
 import { Table } from 'reactstrap';
 
 class Footer extends Component{
@@ -18,17 +18,19 @@ class Footer extends Component{
     const {projetos} = this.state;
 
     return(
-      <div>
-        <table class="table">
-          {projetos.map(projeto => (
-            <tr key={projeto.id}>
-              <td>{projeto.id}</td>
-              <td>{projeto.nome}</td>
-              <td>{projeto.data_criacao}</td>
-            </tr>
+        <div class="d-flex mt-4" style={{marginLeft: "-2rem"}}>
+          {projetos.map (projeto => (
+            <div className='d-flex  ms-5'>
+              <div style={{backgroundColor: "#DBAB4E", height:"100%", width: "16px", marginRight:"-16px", zIndex:"1", borderRadius: "20px 0 0 20px"}}></div>
+              <div class="card" style={{height: "20vh", width: "242px", borderRadius: "20px"}} key={projeto.id}>
+                <div class="card-body" style={{marginLeft: "8px"}}>
+                  <p class="card-text text-center fw-normal">{projeto.nome}</p>
+                  <p class="text-center fw-light" style={{fontSize: "10px", marginTop: "-10%"}}>+ informações</p>
+                </div>
+              </div>
+            </div>
           ))}
-        </table>
-      </div>
+        </div>
     )
   }
 }
