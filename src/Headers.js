c2023855ed02e0bdbe8fde284db134d0e6af4493
Lucassec1 <106749169+Lucassec1@users.noreply.github.com>
2022-06-23 10:14:56 -0300

    Up

--- src/Headers.js
+++ src/Headers.js
@@ -12,9 +12,18 @@ const Headers = (props) => {
 
     return (
         <div> 
-            <Nav className='d-flex flex-column' navbar style={{height:"100vh", width:"100px", backgroundColor:"#4E5C90"}}>
-                <NavItem>
-                    
+            <Nav className='d-flex align-items-center' navbar style={{height:"100vh", width:"100px", backgroundColor:"#4E5C90"}}>
+                <NavItem className='pb-4' style={{marginTop:"183px"}}>
+                    <img src='assets/Nav_projetos.png'></img>
+                </NavItem>
+                <NavItem className='pb-4'>
+                    <img src='assets/Nav_equipes.png'></img>
+                </NavItem> 
+                <NavItem className='pb-4'>
+                    <img src='assets/Nav_pessoas.png'></img>
+                </NavItem> 
+                <NavItem className='pb-4'>
+                    <img src='assets/Nav_tarefas.png'></img>
                 </NavItem> 
             </Nav>
         </div>
