1eff42c5f9df6d407a6c1f15b7d4303bb0ea2fd8
Lucassec1 <106749169+Lucassec1@users.noreply.github.com>
2022-06-24 09:05:36 -0300

    Background-Header

---
+++ src/pages/Projetos/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header () {
+    return (
+        <div className="">
+            <div style={{backgroundColor: "#595959", width: "270px", height: "189px", borderRadius: "15px"}}></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Header;
--- src/pages/background/index.js
+++ src/pages/background/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Background(){
     return (
-        <div className="bg-primary" style={{width:"100vw", height:"100vh", position:"absolute", zIndex:"-1"}}>
+        <div className="bg-primary" style={{width:"100vw", height:"100vh", position:"absolute", zIndex:"-1", background: "linear-gradient(112.68deg, #E0B64B 0%, #FFB649 0.01%, #7296F3 100%)"}}>
 
         </div>
     );
