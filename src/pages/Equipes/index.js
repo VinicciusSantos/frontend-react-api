18d9a85fd0b0dc0bd61f018fbff8e3cfeacefd56
larissacard <larissak.cardosom@gmail.com>
2022-06-24 09:26:01 -0300

    up background

--- src/pages/Equipes/index.js
+++ src/pages/Equipes/index.js
@@ -4,9 +4,9 @@ import Background from "../background";
 
 function Equipes(){
     return (
-        <div className="d-flex">
+        <div className="d-flex justify-content-center align-items-center" style={{width: "100vw", height: "100vh"}}>
 
-            <div className="flex-columun">
+            <div className="" style={{width: "90vw", height: "90vh", backgroundImage: "linear-gradient(113.43deg, #FFB74A -71.14%, #505050 16.53%, #595959 89.77%, #7196F3 141.93%)", boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)", borderRadius: "20px"}}>
                 <h1>Pagina de Equipes</h1>
                
             </div>
--- src/pages/Pessoas/index.js
+++ src/pages/Pessoas/index.js
@@ -4,8 +4,9 @@ import Background from "../background";
 
 function Pessoas(){
     return (
-        <div className="d-flex">
-            <div className="flex-columun">
+        <div className="d-flex justify-content-center align-items-center" style={{width: "100vw", height: "100vh"}}>
+
+            <div className="" style={{width: "90vw", height: "90vh", backgroundImage: "linear-gradient(113.43deg, #FFB74A -71.14%, #505050 16.53%, #595959 89.77%, #7196F3 141.93%)", boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)", borderRadius: "20px"}}>
                 <h1>Pagina de Pessoas</h1>
     
             </div>
