d2084532a52ae7158799ba0de24c754fad32430f
larissacard <larissak.cardosom@gmail.com>
2022-06-27 15:45:27 -0300

    up - modal

--- src/pages/Projetos_Id/menu1.js
+++ src/pages/Projetos_Id/menu1.js
@@ -2,6 +2,7 @@ import React from "react";
 import Data from "./Titulo";
 import Tarefas from "./Tarefas";
 
+
 function Menu1() {
     return (
         <div style={{padding:"20px"}}>
@@ -19,7 +20,25 @@ function Menu1() {
             <div className="cont_tarefasdoprojeto">
                 <div className="Title_exibirTarefasProjeto">
                     <h3>Tarefas do Projeto</h3>
-                    <button className="btn_primary">Novo</button>
+                    <button className="btn_primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Novo</button>
+
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Cadastro de um novo projeto</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
                 </div>
                 <div className="cont_table">
                     <Tarefas />
